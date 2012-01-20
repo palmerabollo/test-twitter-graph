86338513ebf2367b6aad3b5f7560989c11a7551c
Guido García <palmerabollo@gmail.com>
2012-01-20 08:37:57 +0100

    customizable starting node

--- src/main/java/es/guido/twitter/graph/TwitterGrapherBot.java
+++ src/main/java/es/guido/twitter/graph/TwitterGrapherBot.java
@@ -3,14 +3,18 @@ package es.guido.twitter.graph;
 import java.io.IOException;
 
 public class TwitterGrapherBot {
-    private static final String STARTING_NODE = "palmerabollo";
+    private static final String DEFAULT_STARTING_NODE = "palmerabollo";
     private static final int MAX_DEPTH = 1;
     
     private static final String EXPORT_FORMAT = "pdf";
     private static final String EXPORT_PATH = System.getProperty("java.io.tmpdir") + "twitter-graph.pdf";
     
     public static void main(String[] args) throws IOException {
-        TwitterTraverser traverser = new TwitterTraverser(STARTING_NODE, MAX_DEPTH);
+        String startingNode = DEFAULT_STARTING_NODE;
+        if (args.length > 0) {
+            startingNode = args[0];
+        }
+        TwitterTraverser traverser = new TwitterTraverser(startingNode, MAX_DEPTH);
         traverser.traverse();
         traverser.export(EXPORT_FORMAT, EXPORT_PATH);
     }

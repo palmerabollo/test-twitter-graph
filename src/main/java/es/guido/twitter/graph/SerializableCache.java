642e421d59ed76462a50dc9be2a76f0d6f32c7cc
Guido García <palmerabollo@gmail.com>
2012-01-28 15:20:56 +0100

    remove wrong header

--- src/main/java/es/guido/twitter/graph/SerializableCache.java
+++ src/main/java/es/guido/twitter/graph/SerializableCache.java
@@ -1,15 +1,3 @@
-/*
-
-(c) Copyright 2011 Telefonica, I+D. Printed in Spain (Europe). All Rights
-Reserved.
-
-The copyright to the software program(s) is property of Telefonica I+D.
-The program(s) may be used and or copied only with the express written
-consent of Telefonica I+D or in accordance with the terms and conditions
-stipulated in the agreement/contract under which the program(s) have
-been supplied.
-
-*/
 package es.guido.twitter.graph;
 
 import java.io.File;
